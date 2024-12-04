46cdf5852495ddd2fb38a542a96f251802c5361a
zaber <20830726+ZaberKo@users.noreply.github.com>
2024-12-04 15:27:17 +0800

    upload linux scripts & update readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ai-win-builder
-A script to setup up the AI environments on windows.
+
+An interactive script for the setup of AI environments on Windows
+
+TODO list:
+
+- [ ] WSL setup
+  - [ ] Detect Windows versions
+  - [ ] Detect Nvidia driver versions
+  - [ ] Select Linux Distro (default Ubuntu 24.04)
+- [ ] Linux setup
+  - [x] common packages
+  - [x] conda envs
+  - [ ] pyenv w/ venv
---
+++ ubuntu_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+envs=$@
+
+install_packages() {
+    apt-get update
+    apt-get install -y git tmux neovim zsh pipx
+}
+
+install_miniforge() {
+    wget -O Miniforge3.sh  "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+    bash Miniforge3.sh -b
+    rm Miniforge3.sh
+    ~/miniforge3/bin/mamba init
+
+    source "${HOME}/conda/etc/profile.d/conda.sh"
+    # For mamba support also run the following command
+    source "${HOME}/conda/etc/profile.d/mamba.sh"
+}
+
+install_ai_envs() {
+    for env in $envs; do
+        if [ env == "torch"]; then mamba create env -f torch/torch.yaml; fi
+        if [ env == "jax" ]; then mamba create env -f jax/jax.yaml; fi
+    done
+}
